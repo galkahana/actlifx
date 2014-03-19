569665d0974bdf62ac6073714531d23aa010604f
gal kahana <gal.kahana@hotmail.com>
2014-03-19 21:30:37 +0200

    now with the files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app     = express();
+var lifxutils = require('./lifxUtilities');
+
+app.use(express.bodyParser());
+
+app.post('/lifx', function(req, res) {
+  	res.send('executing action"' + req.body.action + '".');
+
+  	var lifxRunner = new lifxutils.LIFXRunner(req.body.action.split(/[ ]+/));
+	lifxRunner.actLIFX();
+});
+
+var port = process.env.PORT || 3000;
+
+app.use('/',express.static('website'))
+app.listen(port, function() {
+  console.log('Server running at',port);
+});
